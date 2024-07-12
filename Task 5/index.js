6b576649d4316223877a1ed398323dd811cfb468
vikas-bishnoi-zeus <Vikas.Bishnoi@zeuslearning.com>
2024-07-12 17:49:57 +0530

    Added Sum and avergae and slected area feature

--- Task 5/index.js
+++ Task 5/index.js
@@ -1,107 +1,182 @@
-class cell{
-  constructor(x,y,value,isSelected){
-    this.x=x;
-    this.y=y;
+const canvas = document.getElementById("spreadsheet");
+const ctx = canvas.getContext("2d");
+
+class cell {
+  constructor(x, y, value, isSelected) {
+    this.x = x;
+    this.y = y;
+    this.value = value;
+    this.isSelected = isSelected;
+  }
+  setValue(value){
     this.value=value;
-    this.isSelected=isSelected;
+  }
+  rectDraw(w, h) {
+    ctx.rect(this.x, this.y, w, h);
+    ctx.fillStyle = "black";
+    ctx.fillText(this.value, this.x + 5, this.y + 18);
+    ctx.stroke();
+    if(this.isSelected){
+      this.selectCell();
+    }
+  }
+  selectCell(){
+    
+    if (this.isSelected) {
+      ctx.fillStyle = 'rgba(0, 100, 255, 0.3)';
+      ctx.fillRect(this.x, this.y, 100, 30);
+    } else {
+      console.log(`This log that selectCell is working for ${Math.floor(this.x/100)} and ${Math.floor(this.y/30)}  ${this.isSelected}`);
+      console.log("Calling rect from select")
+      ctx.clearRect(this.x, this.y,100,30);
+      this.rectDraw(100,30)
+    }
   }
 }
 
 var colWidths = [];
-var rowHeights=[];
-var cells=[];
-function fill(array,length,totalLength){
-  var count=0;
-  let i = 0
-  for (;i<totalLength; i=i+length) {
-    array[count]=i;
+var rowHeights = [];
+var cells = [];
+function fill(array, length, totalLength) {
+  var count = 0;
+  let i = 0;
+  for (; i < totalLength; i = i + length) {
+    array[count] = i;
     count++;
   }
-  array[count]=i;
+  array[count] = i;
 }
-function cellHeightWidth(){
-  const canvas = document.getElementById("spreadsheet");
+function cellHeightWidth() {
   var heightCanvas = canvas.clientHeight;
   var widthCanvas = canvas.clientWidth;
-  fill(rowHeights,30,heightCanvas);
-  fill(colWidths,100,widthCanvas);
+  fill(rowHeights, 30, heightCanvas);
+  fill(colWidths, 100, widthCanvas);
 }
 cellHeightWidth();
-function rectDraw(ctx,curCell,w,h){
-  // console.log(curCell.x,curCell.y);
-  ctx.rect(curCell.x,curCell.y,w,h);
-  ctx. fillText(curCell.value, curCell.x+(w/2),curCell.y+(h/2))
-  ctx.stroke();
-}
+
 function drawGrid() {
-  const canvas = document.getElementById("spreadsheet");
-
-  const ctx = canvas.getContext("2d");
-  ctx.textAlign="center"; 
+  // ctx.textAlign="center";
   ctx.textBaseline = "middle";
-  ctx.font="20px";
+  ctx.font = "20px";
 
-  for (let i = 0;i <rowHeights.length-1; i++) {
-    cells[i]=[];
-    // console.log("I "+ i);
-    for(let j=0;j<colWidths.length-1;j++){
-      let val=Math.floor(Math.random() * 1000);
-      
-      // console.log("j "+ j);
-      const curCell=new cell(colWidths[j],rowHeights[i],val,false);
-      cells[i][j]=curCell;
-      rectDraw(ctx,cells[i][j],colWidths[j+1]-colWidths[j],rowHeights[i+1]-rowHeights[i]);
+  var bl = false;
+  for (let i = 0; i < rowHeights.length - 1; i++) {
+    cells[i] = [];
+    for (let j = 0; j < colWidths.length - 1; j++) {
+      let val = Math.floor(Math.random() * 1000);
+
+      const curCell = new cell(colWidths[j], rowHeights[i], val, bl);
+      cells[i][j] = curCell;
+      cells[i][j].rectDraw(
+        colWidths[j + 1] - colWidths[j],
+        rowHeights[i + 1] - rowHeights[i]
+      );
     }
   }
 }
 drawGrid();
 
-let canvasElem = document.querySelector("canvas");
+let canvasElement = document.querySelector("canvas");
 
-canvasElem.addEventListener("click",(event)=>getInput(event)); 
-function setInput(top,left){
-  var cellInput=document.getElementById("content");
-  var cnleft=left-15;
-  var cntop=top-15;
-  var i=Math.floor(cntop/30);
-  var j=Math.floor(cnleft/100);
-  cellInput.value =cells[i][j].value;
-  console.log(cells[i][j].value)
+canvasElement.addEventListener("click", (event) => getInput(event));
+function setInput(top, left) {
+  var cellInput = document.getElementById("content");
+  var cnleft = left - 15;
+  var cntop = top - 15;
+  var i = Math.floor(cntop / 30);
+  var j = Math.floor(cnleft / 100);
+  cellInput.value = cells[i][j].value;
   cellInput.style.display = "block";
-  cellInput.style.top =top  + 'px';
-  cellInput.style.left = left+ 'px';
-  cellInput.style.height = (30-2.5) + 'px';
-  cellInput.style.width =(100-2.5) + 'px';
-  const canvas = document.getElementById("spreadsheet");
-  const ctx = canvas.getContext("2d");
-  console.log(left,top);
-  
-  ctx.clearRect(left-14,top-14, 98, 18);
+  cellInput.style.top = top + "px";
+  cellInput.style.left = left + "px";
+  cellInput.style.height = 30 - 2.5 + "px";
+  cellInput.style.width = 100 - 2.5 + "px";
 
   cellInput.focus();
-  cellInput.onblur=()=>{
-    console.log(cellInput.value);
-    
-    cells[i][j].value=cellInput.value;
-    ctx.clip();
-    ctx.fillText(cells[i][j].value, cnleft+(100/2) ,cntop+(30/2));
-    cellInput.value=""; 
-  }
+  cellInput.onblur = () => {
+    cells[i][j].setValue(cellInput.value);
+    for (let k = 0; k < cells[i].length; k++) {
+      ctx.clearRect(colWidths[k], rowHeights[i], 100, 30);
+      cells[i][k].rectDraw(100, 30);
+    }
+
+  };
 }
 function getInput(e) {
-  // getPoint(canvasElem, e);
-  // console.log("Hello")
-  let rect = canvasElem.getBoundingClientRect();
+  let rect = canvasElement.getBoundingClientRect();
   let x = e.clientX - rect.left;
   let y = e.clientY - rect.top;
-  let top=((Math.floor(y/30))*30)+15;
-  let left=((Math.floor(x/100))*100)+15;
-  // console.log(top+" "+(x/colWidth));
-  setInput(top,left); 
-  // console.log();
+  let top = Math.floor(y / 30) * 30 + 15;
+  let left = Math.floor(x / 100) * 100 + 15;
+  setInput(top, left);
 }
 
+let selected = [];
+canvasElement.addEventListener("mousedown", (event) => {
+  select(event);
+});
+function select(event) {
+  let rect = canvasElement.getBoundingClientRect();
+  let xind = getXYind(event.clientX-rect.left, 100);
+  let yind = getXYind(event.clientY-rect.top, 30);
+  console.log("Move");
+  mouseMove(xind, yind);
+}
+function getXYind(clientXY, div) {
+  let len = clientXY;
+  return Math.floor(len / div);
+}
+const mouseMove = (xInd, yInd) => {
+  let newXind = -1;
+  let newYind = -1;
+  canvasElement.addEventListener("mousemove", move);
+  function move(event) {
+    let rect = canvasElement.getBoundingClientRect();
 
+    let newXind1 = getXYind(event.clientX-rect.left, 100);
+    let newYind1 = getXYind(event.clientY-rect.top, 30);
+    if(newXind==newXind1 && newYind==newYind1){
+      return;
+    }
+    else{
+      newXind=newXind1;
+      newYind=newYind1;
+    }
+    for(let i=0;i<selected.length;i++){
+      selected[i].isSelected=false;
+      selected[i].selectCell();
+      console.log(i,selected[i]);
+    }
+    selected=[];
+    let sum=0;
+    let count=0;
+    for(let i=Math.min(xInd,newXind);i<=Math.max(xInd,newXind);i++){
+      for(let j=Math.min(yInd,newYind);j<=Math.max(yInd,newYind);j++){
+        cells[j][i].isSelected=true;
+        selected.push(cells[j][i]);
+        cells[j][i].selectCell();
+        let number=Number(cells[j][i].value);
+        if(!Number.isNaN(number)){
+          sum+=number;
+          count++;
+        }
+        // console.log(i+" J "+j);
+      }
+    }
+    document.getElementById("sum").innerHTML=sum;
+    let avr=sum/count;
+    document.getElementById("average").innerHTML=avr;
+  }
+  canvasElement.addEventListener("mouseup", (event) => {
+    console.log("hello");
+    canvasElement.removeEventListener("mousemove", move);
+  });
+};
+
+// canvasElement.addEventListener("mouseup",(event)=>{slectup(event)})
+// function slectup(event){
+//   console.log("up")
+// }
 
 // function getPoint(canvas, event) {
 //   let rect = canvas.getBoundingClientRect();
@@ -112,7 +187,7 @@ function getInput(e) {
 //       "Coordinate y: " + y);
 //   var input=document.getElementById("content");
 //   input.style.position="absolute";
-  
+
 //   input.style.display= "inline-block";
 //   input.style.top=y+"px";
 //   input.style.left=x+"px";
@@ -120,5 +195,3 @@ function getInput(e) {
 //   input.style.width=colWidth-10+"px";
 //   // console.log(input.style.display==='none');
 // }
-
-
